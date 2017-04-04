339cb62e1def2d8054aa40364e6b478195b7115b
SLupov <stoyan.lupov@gmail.com>
2017-04-05 00:49:18 +0300

    Initial migration readded
    In order for the others to create the database successfully

---
+++ SoftCinema/SoftCinema.Data/Migrations/201704041847149_Initial.cs
@@ -0,0 +1,145 @@
+﻿namespace SoftCinema.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Actors",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                    Rating = c.Single(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Length = c.Int(nullable: false),
+                    Rating = c.Single(nullable: false),
+                    Synopsis = c.String(),
+                    DirectorName = c.String(nullable: false),
+                    ReleaseYear = c.Int(nullable: false),
+                    ReleaseCountry = c.String(),
+                    AgeRestriction = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.Auditoriums",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Number = c.Byte(nullable: false),
+                    Schema = c.String(nullable: false),
+                    CinemaId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cinemas", t => t.CinemaId, cascadeDelete: true)
+                .Index(t => t.CinemaId);
+
+            CreateTable(
+                "dbo.Cinemas",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false),
+                    Town = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.Shows",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Start = c.DateTime(nullable: false),
+                    End = c.DateTime(nullable: false),
+                    AuditoriumId = c.Int(nullable: false),
+                    MovieId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Auditoriums", t => t.AuditoriumId, cascadeDelete: true)
+                .ForeignKey("dbo.Movies", t => t.MovieId, cascadeDelete: true)
+                .Index(t => t.AuditoriumId)
+                .Index(t => t.MovieId);
+
+            CreateTable(
+                "dbo.Tickets",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.Int(nullable: false),
+                    ShowId = c.Int(nullable: false),
+                    Row = c.Byte(nullable: false),
+                    Col = c.Byte(nullable: false),
+                    Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    Type = c.String(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.Shows", t => t.ShowId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.ShowId);
+
+            CreateTable(
+                "dbo.Users",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    PasswordHash = c.Binary(nullable: false),
+                    Phone = c.String(),
+                    Email = c.String(nullable: false),
+                    Role = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.MovieActors",
+                c => new
+                {
+                    Movie_Id = c.Int(nullable: false),
+                    Actor_Id = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => new { t.Movie_Id, t.Actor_Id })
+                .ForeignKey("dbo.Movies", t => t.Movie_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Actors", t => t.Actor_Id, cascadeDelete: true)
+                .Index(t => t.Movie_Id)
+                .Index(t => t.Actor_Id);
+
+        }
+
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tickets", "ShowId", "dbo.Shows");
+            DropForeignKey("dbo.Tickets", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Shows", "MovieId", "dbo.Movies");
+            DropForeignKey("dbo.Shows", "AuditoriumId", "dbo.Auditoriums");
+            DropForeignKey("dbo.Auditoriums", "CinemaId", "dbo.Cinemas");
+            DropForeignKey("dbo.MovieActors", "Actor_Id", "dbo.Actors");
+            DropForeignKey("dbo.MovieActors", "Movie_Id", "dbo.Movies");
+            DropIndex("dbo.MovieActors", new[] { "Actor_Id" });
+            DropIndex("dbo.MovieActors", new[] { "Movie_Id" });
+            DropIndex("dbo.Tickets", new[] { "ShowId" });
+            DropIndex("dbo.Tickets", new[] { "UserId" });
+            DropIndex("dbo.Shows", new[] { "MovieId" });
+            DropIndex("dbo.Shows", new[] { "AuditoriumId" });
+            DropIndex("dbo.Auditoriums", new[] { "CinemaId" });
+            DropTable("dbo.MovieActors");
+            DropTable("dbo.Users");
+            DropTable("dbo.Tickets");
+            DropTable("dbo.Shows");
+            DropTable("dbo.Cinemas");
+            DropTable("dbo.Auditoriums");
+            DropTable("dbo.Movies");
+            DropTable("dbo.Actors");
+        }
+    }
+}
