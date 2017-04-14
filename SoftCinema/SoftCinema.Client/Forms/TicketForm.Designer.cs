aed90f935ae227513aef20abb59b2734cedd1f72
Maria Velikova <maria.y.velikova@gmail.com>
2017-04-14 15:36:23 +0300

    TicketForm changes

--- SoftCinema/SoftCinema.Client/Forms/TicketForm.Designer.cs
+++ SoftCinema/SoftCinema.Client/Forms/TicketForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SoftCinema.Client.Forms
 {
-    partial class Ticket
+    partial class TicketForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,17 +29,19 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.cinemaLabel = new System.Windows.Forms.Label();
+            this.screeningLabel = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.seatLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.townLabel = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -50,26 +52,26 @@
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
             // 
-            // label1
+            // cinemaLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label1.Location = new System.Drawing.Point(144, 92);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 18);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Cinema:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.cinemaLabel.AutoSize = true;
+            this.cinemaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.cinemaLabel.Location = new System.Drawing.Point(144, 92);
+            this.cinemaLabel.Name = "cinemaLabel";
+            this.cinemaLabel.Size = new System.Drawing.Size(63, 18);
+            this.cinemaLabel.TabIndex = 1;
+            this.cinemaLabel.Text = "Cinema:";
+            this.cinemaLabel.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // screeningLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label2.Location = new System.Drawing.Point(144, 194);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Screening:";
+            this.screeningLabel.AutoSize = true;
+            this.screeningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.screeningLabel.Location = new System.Drawing.Point(144, 194);
+            this.screeningLabel.Name = "screeningLabel";
+            this.screeningLabel.Size = new System.Drawing.Size(78, 18);
+            this.screeningLabel.TabIndex = 2;
+            this.screeningLabel.Text = "Screening:";
             // 
             // comboBox2
             // 
@@ -79,15 +81,15 @@
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 3;
             // 
-            // label3
+            // dateLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label3.Location = new System.Drawing.Point(144, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 18);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Date:";
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.dateLabel.Location = new System.Drawing.Point(144, 146);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(43, 18);
+            this.dateLabel.TabIndex = 4;
+            this.dateLabel.Text = "Date:";
             // 
             // comboBox3
             // 
@@ -97,15 +99,15 @@
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 5;
             // 
-            // label4
+            // seatLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label4.Location = new System.Drawing.Point(144, 246);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 18);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Seat:";
+            this.seatLabel.AutoSize = true;
+            this.seatLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.seatLabel.Location = new System.Drawing.Point(144, 246);
+            this.seatLabel.Name = "seatLabel";
+            this.seatLabel.Size = new System.Drawing.Size(42, 18);
+            this.seatLabel.TabIndex = 6;
+            this.seatLabel.Text = "Seat:";
             // 
             // button1
             // 
@@ -116,15 +118,15 @@
             this.button1.Text = "Purchase";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // totalPriceLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label5.Location = new System.Drawing.Point(144, 300);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Total price:";
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.totalPriceLabel.Location = new System.Drawing.Point(144, 300);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(79, 17);
+            this.totalPriceLabel.TabIndex = 9;
+            this.totalPriceLabel.Text = "Total price:";
             // 
             // textBox1
             // 
@@ -152,25 +154,46 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "// TODO: Visualize the audithorium schema with its seats";
             // 
-            // Ticket
+            // townLabel
+            // 
+            this.townLabel.AutoSize = true;
+            this.townLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.townLabel.Location = new System.Drawing.Point(144, 36);
+            this.townLabel.Name = "townLabel";
+            this.townLabel.Size = new System.Drawing.Size(49, 18);
+            this.townLabel.TabIndex = 6;
+            this.townLabel.Text = "Town:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(255, 37);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 13;
+            // 
+            // TicketForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 398);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.townLabel);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.totalPriceLabel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.seatLabel);
             this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.screeningLabel);
+            this.Controls.Add(this.cinemaLabel);
             this.Controls.Add(this.comboBox1);
-            this.Name = "Ticket";
+            this.Name = "TicketForm";
             this.Text = "Ticket";
+            this.Load += new System.EventHandler(this.Ticket_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,16 +202,18 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label cinemaLabel;
+        private System.Windows.Forms.Label screeningLabel;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label dateLabel;
         private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label seatLabel;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label totalPriceLabel;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label townLabel;
+        private System.Windows.Forms.ComboBox comboBox4;
     }
 }
